e85814e7a93f91b39e8a688dd386b0fae7f75f80
Diwan Malla <diwanmalla@192-168-1-4.tpgi.com.au>
2024-10-06 16:43:01 +1100

    Added some component in add blog page

--- BACKEND/components/Blog.js
+++ BACKEND/components/Blog.js
@@ -1,14 +1,117 @@
-
-import ReactMarkdown from 'react-markdown';
-import MarkdownEditor from 'react-markdown-editor-lite';
-import 'react-markdown-editor-lite/lib/index.css';
+import ReactMarkdown from "react-markdown";
+import MarkdownEditor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
+import Spinner from "./Spinner";
 
 export default function Blog() {
-    
+  return (
+    <>
+      <form className="addWebsiteform">
+        {/*blog title*/}
+        <div className="w-100 flex flex-col flex-left mb-2">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" placeholder="Enter small title" />
+        </div>
 
+        {/*blog slug url*/}
+        <div className="w-100 flex flex-col flex-left mb-2">
+          <label htmlFor="slug">Slug (seo friendly url)</label>
+          <input type="text" id="slug" placeholder="Enter slug url" />
+        </div>
+        {/*blog category*/}
+        <div className="w-100 flex flex-col flex-left mb-2">
+          <label htmlFor="category">
+            Select category (for multi select press ctrl + mouse left key)
+          </label>
+          <select name="category" id="category" multiple>
+            <option value="Node js">Node Js</option>
+            <option value="React js">React Js</option>
+            <option value="Next js">Next Js</option>
+            <option value="CSS">CSS</option>
+            <option value="Digital Marketing">Digital Marketing</option>
+            <option value="Flutter Dev">Flutter Dev</option>
+            <option value="Database">Database</option>
+            <option value="Deployment">Deployment</option>
+          </select>
+        </div>
+        {/*blog image*/}
+        <div className="w-100 flex flex-col flex-left mb-2">
+          <div className="w-100">
+            <label>
+              Images (first image will be show as thumbnail, you can drag)
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              className="mt-1"
+              accept="image/"
+              multiple
+            />
+          </div>
+          <div className="w-100 flex flex-left mt-1">
+            <Spinner />
+          </div>
+        </div>
 
-    return <>
+        {/*Image preview and image sortable*/}
+        {/*pending*/}
+        {/*markdown description*/}
+        <div className="description w-100 flex flex-col flex-left mb-2">
+          <label htmlFor="description" className="w-100">
+            Blog Content (for image: first upload and copy link and paste in
+            ![alt text](link))
+            <MarkdownEditor
+              className="w-100"
+              style={{ height: "400px" }} //you can adjust the height as needed
+              renderHTML={(text) => (
+                <ReactMarkdown
+                  components={{
+                    code: ({ node, inline, className, children, ...props }) => {
+                      //for code
+                      const match = /language-(\w+)/.exec(className || "");
 
+                      if (inline) {
+                        return <code>{children}</code>;
+                      } else if (match) {
+                        return (
+                          <div style={{ position: "relative" }}>
+                            <pre
+                              style={{
+                                padding: "0",
+                                borderRadius: "5px",
+                                overflowX: "auto",
+                                whiteSpace: "pre-wrap",
+                              }}
+                              {...props}
+                            >
+                              <code>{children}</code>
+                            </pre>
+                            <button
+                              style={{
+                                position: "absolute",
+                                top: "0",
+                                zIndex: "1",
+                              }}
+                              onClick={() =>
+                                navigator.clipboard.writeText(children)
+                              }
+                            >
+                              Copy code
+                            </button>
+                          </div>
+                        );
+                      }
+                      return null;
+                    },
+                  }}
+                >
+                  {text}
+                </ReactMarkdown>
+              )}
+            />{" "}
+          </label>
+        </div>
+      </form>
     </>
+  );
 }
-
--- BACKEND/pages/blogs/addblog.js
+++ BACKEND/pages/blogs/addblog.js
@@ -1,3 +1,6 @@
+//icon import
+import Blog from "@/components/Blog";
+import { SiBloglovin } from "react-icons/si";
 export default function Addblog() {
   return (
     <>
@@ -7,7 +10,16 @@ export default function Addblog() {
             <h2>
               Add <span>Blog</span>
             </h2>
+            <h3>ADMIN PANEL</h3>
           </div>
+          <div className="breadcrumb">
+            <SiBloglovin />
+            <span>/</span>
+            <span>Addblog</span>
+          </div>
+        </div>
+        <div className="blogsadd">
+          <Blog />
         </div>
       </div>
     </>

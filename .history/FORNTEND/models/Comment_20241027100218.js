653fcd82bcc19b8a416212a565dc73bc0fe0fecd
Diwan Malla <diwanmalla@192-168-1-16.tpgi.com.au>
2024-10-27 10:39:35 +1100

    Update .gitignore to ignore .history folder

--- .history/FORNTEND/models/Comment_20241027100218.js
+++
@@ -1,8 +0,0 @@
-import { Schema, models, model } from "mongoose";
-const BlogSchema = new Schema(
-  {},
-  {
-    timestamps: true, //this will automatically manage createdAt and updatedAt
-  }
-);
-export const Blog = models.Blog || model("Blog", BlogSchema, "blogs");
--- .history/FORNTEND/pages/api/blogs/index_20241027100643.js
+++
@@ -1,7 +0,0 @@
-import { mongooseConnect } from "@/lib/mongoose";
-import { Blog } from "@/models/Blog";
-
-export default async function handle(req, res) {
-  //if authenticated, connect to mongodb
-  await mongooseConnect();
-}
